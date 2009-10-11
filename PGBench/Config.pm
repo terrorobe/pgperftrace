20b320f4a1279dc971fe7db884ba1ad9b6954b27
Michael Renner <michael.renner@amd.co.at>
2009-10-11 03:18:49 +0200

    small config file changes

--- PGBench/Config.pm
+++ PGBench/Config.pm
@@ -1,24 +1,12 @@
 package Config;
 
-#use Moose;
-#
-#has 'bench_root_dir' => (is => 'ro', isa => 'ExistingDir', required => 1, lazy_build => 1);
-#
-#
-## FIXME: Needs moar configparsing!
-#
-#sub _build_bench_root_dir {
-#    print "constructed!\n";
-#    return '/srv/pgbenchroot';
-#}
-
-#Moose fails us.
+# FIXME: This could use a singleton and/or a configparser with a nice config file
 
 use vars (%opt);
 
 %opt = (
         bench_root_dir => '/srv/pgbenchroot',
-        scratch_dir => '/srv/raid0',
+        scratch_dir => '/srv/raid0/pgbenchscratch',
         sysbench_dir => '/opt/sysbench/bin',
        );
 
