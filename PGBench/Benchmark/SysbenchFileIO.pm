5ae8a45495850ff4d0890d075e8e58b124474440
Michael Renner <michael.renner@amd.co.at>
2009-10-15 00:19:06 +0200

    comments!

--- PGBench/Benchmark/SysbenchFileIO.pm
+++ PGBench/Benchmark/SysbenchFileIO.pm
@@ -7,5 +7,12 @@ with 'PGBench::Benchmark::Sysbench';
 # We almost certainly want to use 
 # --file-extra-flags=direct --file-block-size=8192
 
+#robe@testbox:/srv/raid0/sysbench$ /opt/sysbench/bin/sysbench --num-threads=8 --max-requests=10000 --test=fileio --file-extra-flags=direct --file-block-size=8192 --file-test-mode=rndrd run &
+#
+#
+#Operations performed:  10000 Read, 0 Write, 0 Other = 10000 Total
+#Read 78.125Mb  Written 0b  Total transferred 78.125Mb  (3.5503Mb/sec)
+#      454.44 Requests/sec executed
+#
 
 1;
