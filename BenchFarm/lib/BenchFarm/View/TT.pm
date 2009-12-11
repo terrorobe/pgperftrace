b4c4d45fed74572a7d30c8526a69fa42bd478689
robe@pgtest.local <robe@pgtest.local>
2009-12-11 12:16:31 +0100

    boilerplate TT view

---
+++ BenchFarm/lib/BenchFarm/View/TT.pm
@@ -0,0 +1,31 @@
+package BenchFarm::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+BenchFarm::View::TT - TT View for BenchFarm
+
+=head1 DESCRIPTION
+
+TT View for BenchFarm. 
+
+=head1 AUTHOR
+
+=head1 SEE ALSO
+
+L<BenchFarm>
+
+robe,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
