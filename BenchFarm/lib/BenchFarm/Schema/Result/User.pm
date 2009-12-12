cfc895130b7729f7299a1a33c61456388b610ee6
Michael Renner <michael.renner@amd.co.at>
2009-12-12 23:22:14 +0100

    Fix quoting for reserved keyword "user"

--- BenchFarm/lib/BenchFarm/Schema/Result/User.pm
+++ BenchFarm/lib/BenchFarm/Schema/Result/User.pm
@@ -6,7 +6,7 @@ use warnings;
 use base 'DBIx::Class';
 
 __PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "Core");
-__PACKAGE__->table("user");
+__PACKAGE__->table('"user"');
 __PACKAGE__->add_columns(
   "id",
   {
